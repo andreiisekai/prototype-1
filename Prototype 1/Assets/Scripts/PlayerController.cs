1b32f5505cf3e4d3e2f0cd0b848e34fbfe67f363
internetkun <internetkundev@gmail.com>
2022-02-12 14:20:32 +0200

    Bug fix

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Private variables
-    private float speed = 5.0f;
-    private float turnSpeed;
+    private float speed = 15.0f;
+    private float turnSpeed = 35.0f;
     private float horizontalInput;
     private float forwardInput;
 
